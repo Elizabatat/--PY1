3532515f2aa0c5bd35217298dc3a1f3836f2421e
Elizabatat <115361896+Elizabatat@users.noreply.github.com>
2022-10-16 18:37:36 +0300

    Add files via upload
    Лабораторная работа 3

---
+++ Лабораторная работа 3/task8.py
@@ -0,0 +1,18 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0  # количество месяцев, которое можно прожить
+
+# TODO Оформить решение
+
+delta = salary - spend
+while money_capital + delta >= spend:
+    money_capital += delta
+    spend = spend * (1+increase )
+
+    month += 1
+
+print(month)
+
